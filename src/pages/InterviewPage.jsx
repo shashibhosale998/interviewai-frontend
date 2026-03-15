ec37ce284a2d495aa0ac7877162b314bef5330b2
shashibhosale998 <153603195+shashibhosale998@users.noreply.github.com>
2026-03-15 21:53:40 +0530

    fix speech recognition duplicate text bug

--- src/pages/InterviewPage.jsx
+++ src/pages/InterviewPage.jsx
@@ -38,7 +38,7 @@ function InterviewPage() {
 
         recognition.onresult = (event) => {
             let finalTranscript = '';
-            for (let i = 0; i < event.results.length; i++) {
+            for (let i = event.resultIndex; i < event.results.length; i++) {
                 if (event.results[i].isFinal) {
                     finalTranscript += event.results[i][0].transcript;
                 }
